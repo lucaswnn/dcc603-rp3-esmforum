10a298073ee7130143b79778b73be69bee183858
lucaswnn <lucaswnn@hotmail.com>
2025-06-20 17:20:50 -0300

    testes com cobertura 100

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,35 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de quatro respostas a duas perguntas', () => {
+  const p1 = modelo.cadastrar_pergunta('Pergunta 1?');
+  const p2 = modelo.cadastrar_pergunta('Pergunta 2?');
+  modelo.cadastrar_resposta(p1, 'Resposta 1.1');
+  modelo.cadastrar_resposta(p1, 'Resposta 1.2');
+  modelo.cadastrar_resposta(p2, 'Resposta 2.1');
+  modelo.cadastrar_resposta(p2, 'Resposta 2.2');
+  const respostas_p1 = modelo.get_respostas(p1);
+  const respostas_p2 = modelo.get_respostas(p2);
+
+  // testa quantidade de respostas de cada pergunta
+  expect(respostas_p1.length).toBe(2);
+  expect(respostas_p2.length).toBe(2);
+
+  // testa o texto das respostas
+  expect(respostas_p1[0].texto).toBe('Resposta 1.1');
+  expect(respostas_p1[1].texto).toBe('Resposta 1.2');
+  expect(respostas_p2[0].texto).toBe('Resposta 2.1');
+  expect(respostas_p2[1].texto).toBe('Resposta 2.2');
+});
+
+test('Testando a captura de uma pergunta', () => {
+  const id1 = modelo.cadastrar_pergunta('Pergunta 1?');
+  const id2 = modelo.cadastrar_pergunta('Pergunta 2?');
+  const pergunta1 = modelo.get_pergunta(id1);
+  const pergunta2 = modelo.get_pergunta(id2);
+  expect(pergunta1.id_pergunta).toBe(id1);
+  expect(pergunta1.texto).toBe('Pergunta 1?');
+  expect(pergunta2.id_pergunta).toBe(id2);
+  expect(pergunta2.texto).toBe('Pergunta 2?');
+});
